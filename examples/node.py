90c7595de99d2c08e07a37a79d0549bae671976c
kkisbug <uwastemt@gmail.com>
2023-09-03 12:08:47 +0800

    custom listen port-01

--- examples/node.py
+++ examples/node.py
@@ -20,7 +20,7 @@ def parse_arguments():
     # Optional arguments
     parser.add_argument("-i", "--ip", help="IP address of existing node", type=str, default=None)
     parser.add_argument("-p", "--port", help="port number of existing node", type=int, default=None)
-    parser.add_argument("-l", "--listen", help="listen port this node,default=17168(first node)/17169(second node)", type=int, default=17168)
+    parser.add_argument("-l", "--listen", help="listen port of this node,default=17168(first node)/17169(second node)", type=int, default=17168)
 
     return parser.parse_args()
 
@@ -42,7 +42,7 @@ def connect_to_bootstrap_node(args):
         loop.close()
 
 
-def create_bootstrap_node():
+def create_bootstrap_node(args):
     loop = asyncio.get_event_loop()
     loop.set_debug(True)
 
@@ -63,7 +63,7 @@ def main():
     if args.ip and args.port:
         connect_to_bootstrap_node(args)
     else:
-        create_bootstrap_node()
+        create_bootstrap_node(args)
 
 
 if __name__ == "__main__":
